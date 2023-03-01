dee1db4c642e6de06de5ea4e9bb4d747ae5352e9
EdgarPinto38 <edgarpinto010203@gmail.com>
2023-02-28 20:31:53 -0600

    Arreglando Input

--- Si/Assets/Scripts/Player/PlayerController.cs
+++ Si/Assets/Scripts/Player/PlayerController.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     bool isLeft = false;
     bool isRight = false;
+    bool isUp = false;
 
     public Rigidbody2D rb;
     public float speed;
+    public float jump;
 
 
 
@@ -32,16 +35,48 @@ public class PlayerController : MonoBehaviour
         isRight = false;
     }
 
+    public void clickUp()
+    {
+        isUp = true;
+    }
+
+    public void releaseUp()
+    {
+        isUp = false;
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.AddForce(new Vector3(0, jump, 0) * Time.deltaTime);
+        }
+    }
     private void FixedUpdate()
     {
+        
+
         if (isLeft)
         {
-            rb.transform.position = new Vector2(-speed, 0) * Time.deltaTime;
+            //rb.AddForce(new Vector2(-speed, 0) * Time.deltaTime);
+            rb.transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
         }
 
         if (isRight)
         {
-            rb.transform.position = new Vector2(speed, 0) * Time.deltaTime;
+            //rb.AddForce(new Vector2(speed, 0) * Time.deltaTime);
+            rb.transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
         }
+
+        if (isUp)
+        {
+            //rb.AddForce(new Vector2(speed, 0) * Time.deltaTime);
+            rb.transform.position += new Vector3(0, speed * 2f, 0) * Time.deltaTime;
+        }
+
     }
+
+
+   
 }
